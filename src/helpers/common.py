ee3bb878231e7217e1de50c7175d82cbd6e813ca
Ibrahim Berber <66446655+ibrahimberb@users.noreply.github.com>
2021-09-06 15:13:13 +0300

    Update common.py

--- src/helpers/common.py
+++ src/helpers/common.py
@@ -1,4 +1,6 @@
 import os
+import pandas as pd
+from pandas import DataFrame
 
 
 def print_annotation(s):
@@ -7,3 +9,15 @@ def print_annotation(s):
 
 def get_file_path(project_common_file_dir, filename):
     return os.path.join(project_common_file_dir, filename)
+
+
+def extract_data(filename: str, data: DataFrame, extension: str, overwrite=False):
+    """
+    Extract the given dataframe
+    # todo add time and date.
+    :param overwrite:
+    :param extension:
+    :param filename:
+    :param data:
+    :return:
+    """
