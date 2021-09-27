44e058ca2e745245ce60ce8beb446df5b90cfef7
Ibrahim Berber <66446655+ibrahimberb@users.noreply.github.com>
2021-09-27 23:24:33 +0300

    Update displayers.py
    Added `display_data` function

--- src/helpers/helpers_predator/displayers.py
+++ src/helpers/helpers_predator/displayers.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pandas import DataFrame
 from IPython.display import display
 import seaborn as sns
 
@@ -6,11 +7,22 @@ from ..mylogger import get_handler
 import logging
 
 handler = get_handler()
+handler_simple = get_handler(log_type="simple")
 
 log = logging.getLogger(__name__)
 log.handlers[:] = []
 log.addHandler(handler)
 log.setLevel(logging.DEBUG)
+
+log_simple = logging.getLogger(__name__)
+log_simple.handlers[:] = []
+log_simple.addHandler(handler_simple)
+log_simple.setLevel(logging.DEBUG)
+
+
+def display_data(data: DataFrame, head_row=3):
+    log_simple.debug(f"Data dimensions: {data.shape}")
+    display(data.head(head_row))
 
 
 # TODO: unittest
